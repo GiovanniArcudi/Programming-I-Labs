87296419a0732c51fb40dbafc902c1b057998dd7
Giovanni Arcudi <56199876+GiovanniArcudi@users.noreply.github.com>
2019-11-12 19:01:55 +0000

    Update UserGroup.java

--- lab4/lab4part2/UserGroup.java
+++ lab4/lab4part2/UserGroup.java
@@ -6,14 +6,14 @@
  */
 import java.util.ArrayList;     // Importing the ArrayList library.
 
-public class UserGroup{
+public class UserGroup {
     private ArrayList<User> group;
 
     /**
      * This is the UserGroup class constructor.
      * Every UserGroup created by the constructor will have an ArrayList of Users called "group".
      */
-    public UserGroup(){
+    public UserGroup() {
         group = new ArrayList<User>();
     }
 
@@ -28,7 +28,7 @@ public class UserGroup{
      * When the method addSampleData() is called, it creates 10 Users and using the ArrayList's add() method put the 10
      * new User objects into the "group" ArrayList.
      */
-    public void addSampleData (){
+    public void addSampleData () {
         for(int i=0; i<10; i++) {
             User myUser = new User("test","test","test");
             group.add(myUser);
@@ -39,7 +39,7 @@ public class UserGroup{
      * When the method getUser(int i) is called, it takes an int as a parameter and returns the User in that slot of the
      * ArrayList.
      */
-    public User getUser(int i){
+    public User getUser(int i) {
         return group.get(i);
     }
 
@@ -47,8 +47,8 @@ public class UserGroup{
      *  When the method printUsernames() is called, it loops through the ArrayList using an enhanced for loop and prints
      *  the username and userType of each user in the ArrayList.
      */
-    public void printUsernames(){
-        for(User user : group){
+    public void printUsernames() {
+        for(User user : group) {
             System.out.println(user.getUsername() + " " + user.getUserType());
         }
     }
