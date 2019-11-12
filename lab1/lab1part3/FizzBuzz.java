8da2ef4aa47f207c3be83aa8df80f0e55a57f5a8
Giovanni Arcudi <56199876+GiovanniArcudi@users.noreply.github.com>
2019-11-12 18:50:37 +0000

    Update FizzBuzz.java

--- lab1/lab1part3/FizzBuzz.java
+++ lab1/lab1part3/FizzBuzz.java
@@ -4,24 +4,23 @@
  *
  * @author Giovanni Arcudi
  */
-
 public class FizzBuzz {     //Class wrapper.
 
     public static void main(String[] args) {        //Main method wrapper.
 
         /* Assign "i" the value 1, completes the instructions inside the curly brackets, and then increses the value
         of "i". It repeats this for 60 times. */
-        for(Integer i = new Integer(1); i < 61; i++){
+        for(Integer i = new Integer(1); i < 61; i++) {
 
-            if(i % 3 == 0){     //Checking if "i" is divisible by 3.
+            if(i % 3 == 0) {     //Checking if "i" is divisible by 3.
                 System.out.print("Fizz");       //If true, prints "Fizz".
             }
             
-            if(i % 5 == 0){     //Checking if "i" is divisible by 5.
+            if(i % 5 == 0) {     //Checking if "i" is divisible by 5.
                 System.out.print("Buzz");        //If true, prints "Buzz".
             }
 
-            if(i % 3 != 0 && i % 5 != 0){       //Checking if "i" is not divisible by 3 and 5.
+            if(i % 3 != 0 && i % 5 != 0) {       //Checking if "i" is not divisible by 3 and 5.
                 System.out.print(i);        //If "i" is not divisible by 3 and not divisible by 5, then it prints the value of "i".
             }
 
