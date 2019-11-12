87c8cfb5c2d0e86bb00eea0a30e07e7a99c81ccc
Giovanni Arcudi <56199876+GiovanniArcudi@users.noreply.github.com>
2019-11-12 18:59:10 +0000

    Update SmartCard.java

--- lab3/lab3part3/SmartCard.java
+++ lab3/lab3part3/SmartCard.java
@@ -5,8 +5,7 @@
  *
  * @author Giovanni Arcudi
  */
-
-public class SmartCard{
+public class SmartCard {
     String ownerName;
     boolean staff = false;
 
@@ -14,14 +13,14 @@ public class SmartCard{
      * This is the SmartCard class constructor.
      * It takes a String parameter to set the owner of the card.
      */
-    public SmartCard(String ownerName){
+    public SmartCard(String ownerName) {
         this.ownerName = ownerName;
     }
 
     /**
      * When the method getOwner() is called, it returns the owner of the SmartCard.
      */
-    public String getOwner(){
+    public String getOwner() {
         return ownerName;
     }
 
@@ -29,21 +28,21 @@ public class SmartCard{
      * When the method setStaff(boolean staff) is called, it sets the staff status of the SmartCard to what it has
      * received as a parameter.
      */
-    public void setStaff(boolean staff){
+    public void setStaff(boolean staff) {
         this.staff = staff;
     }
 
     /**
      * When  the method isStaff() is called, it returns whether the card belongs to a member of staff.
      */
-    public boolean isStaff(){
+    public boolean isStaff() {
         return staff;
     }
 
     /**
      * It creates a new SmartCard.
      */
-    public static void main(String[] args){
+    public static void main(String[] args) {
         SmartCard mySmartCard = new SmartCard("Giovanni");
     }
 }
