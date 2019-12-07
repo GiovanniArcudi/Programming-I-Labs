43d97b91d9448eb47e1221e0b7f5706b3e892dca
Giovanni Arcudi <56199876+GiovanniArcudi@users.noreply.github.com>
2019-12-07 21:59:01 +0000

    Update GuessingGame.java

--- lab1/lab1part2/GuessingGame.java
+++ lab1/lab1part2/GuessingGame.java
@@ -3,14 +3,15 @@
  * whether it is too high, too low or exactly the same.
  *
  * @author Giovanni Arcudi
+ * @version 1.0
  */
 public class GuessingGame {
-
+    
     public static void main(String[] args) {
         Integer numberToGuess;
         Integer guessedNumber;
 
-        //Creating a new toolbox.
+        //Creates a new Toolbox.
         Toolbox myToolbox = new Toolbox();
 
         System.out.println("Welcome to Guessing Game");
@@ -18,15 +19,14 @@ public class GuessingGame {
         numberToGuess = myToolbox.getRandomInteger(10);
         guessedNumber = myToolbox.readIntegerFromCmd();
 
-        // Checking whether the guessed number is equal to the number to guess.
+        /* Checks whether the guessed number is equal to the number to guess and prints out if the input number is
+        right, too low or too high. */
         if (numberToGuess.equals(guessedNumber)) {
             System.out.println("right");
         } else {
-            // If the guessed number is not equal to the number to guess then we have to find out if it is greater or smaller.
             if (numberToGuess > guessedNumber) {
                 System.out.println("too low");
             } else {
-                // If the guessed number is not equal to and not smaller than the number to guess, then it must be greater.
                 System.out.println("too high");
             }
         }
