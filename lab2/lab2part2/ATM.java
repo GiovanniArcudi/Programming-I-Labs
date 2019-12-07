257416ca97500095dfaabe9ef3744c7e9ec69cd4
Giovanni Arcudi <56199876+GiovanniArcudi@users.noreply.github.com>
2019-12-07 22:18:22 +0000

    Update ATM.java

--- lab2/lab2part2/ATM.java
+++ lab2/lab2part2/ATM.java
@@ -1,16 +1,18 @@
 /**
- * This program simulates an ATM. After aking the user to enter the balance that they would like in their account,
- * it allows them to withdraw or deposit an amount, inquire about the balance, or quit the ATM.
+ * This program simulates an {@link ATM}. After aking the user to enter the balance that they would like in their account,
+ * it allows them to {@link #withdraw()} or {@link #deposit()} an amount, {@link #inquire()} about the 
+ * {@code balance}, or {@link #quit()} the {@link ATM}.
  * In this version of the program the ATM can only perform one operation before "turning off".
  *
  * @author Giovanni Arcudi
+ * @version 2.0
  */
 public class ATM {
-    Toolbox myToolbox = new Toolbox();      // Creates a new toolbox.
+    Toolbox myToolbox = new Toolbox();
     int balance;
 
     /**
-     * Creates an ATM object and then calls a method called go().
+     * Creates an {@link ATM} object and then calls a method called {@link #go()}.
      */
     public static void main(String[] args) {
         ATM myATM = new ATM();
@@ -18,8 +20,8 @@ public class ATM {
     }
 
     /**
-     * When the method go() is called the program prompts the user to enter the balance that they would like in their
-     * account. Then it shows the menu with all the operations that the user can do.
+     * Prompts the user to enter the {@code balance} that they would like in their account.
+     * Then it shows the menu with all the operations that the user can do.
      */
     public void go() {
         System.out.println("Welcome to online ATM banking");
@@ -47,13 +49,12 @@ public class ATM {
     }
 
     /**
-     * When the method withdraw() is called the program prompts the user to enter the amount that they would like to
-     * withdraw form their account. Once entered the amount, the withdrawal is performed and the new account balance
-     * is showed.
+     * Prompts the user to enter the amount that they would like to withdraw form their account.
+     * Once entered the amount, the withdrawal is performed and the new account balance is showed.
      */
     public void withdraw() {
         System.out.println("*****************************************");
-        System.out.println("              Withdrawal");
+        System.out.println("               Withdrawal");
         System.out.println("*****************************************");
         System.out.println("How much would you like to withdraw?");
 
@@ -67,8 +68,8 @@ public class ATM {
     }
 
     /**
-     * When the method deposit() is called the program prompts the user to enter the amount that they would like to
-     * deposit in their account. Once entered the amount, the deposit is performed and the new account balance is showed.
+     * Prompts the user to enter the amount that they would like to deposit in their account.
+     * Once entered the amount, the deposit is performed and the new account balance is showed.
      */
     public void deposit() {
         System.out.println("*****************************************");
@@ -86,7 +87,7 @@ public class ATM {
     }
 
     /**
-     * When the method inquire() is called the account balance is showed.
+     * Shows the account {@code balance}.
      */
     public void inquire() {
         System.out.println("*****************************************");
@@ -95,7 +96,7 @@ public class ATM {
     }
 
     /**
-     * When the method quit() is called the ATM says goodbye to the customer and stops working.
+     * Says goodbye to the customer and quits.
      */
     public void quit() {
         System.out.println("*****************************************");
