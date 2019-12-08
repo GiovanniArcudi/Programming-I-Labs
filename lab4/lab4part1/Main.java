1b0328091f5696ba1de971e5c7edda66d261681d
Giovanni Arcudi <56199876+GiovanniArcudi@users.noreply.github.com>
2019-12-08 15:37:50 +0000

    Update Main.java

--- lab4/lab4part1/Main.java
+++ lab4/lab4part1/Main.java
@@ -1,10 +1,11 @@
 /**
- * This program: (I) prints a times table and (II) calculates how many successive integers to add to make a total over 500.
- * (I) The user is asked to enter a number and the program outputs a multiplication table up to 20 times that number.
- * (II) After having summed consecutive integers starting from 1 until the sum of those integers is greater than 500,
- * the program prints out how many iterations were required to do this.
+ * This program prints a times table and calculates how many successive integers to add to make a total over 500. <ol>
+ * <li>The user is asked to enter a number and the program outputs a multiplication table up to 20 times that number.</li>
+ * <li>After having summed consecutive integers starting from 1 until the sum of those integers is greater than 500,
+ * the program prints out how many iterations were required to do this.</li></ol>
  *
  * @author Giovanni Arcudi
+ * @version 1.0
  */
 public class Main {
 
@@ -14,12 +15,13 @@ public class Main {
     public static void main(String[] args) {
         Toolbox myToolbox = new Toolbox();      // Creates a new toolbox.
 
-        int userNumber = myToolbox.readIntegerFromCmd();        // The user is asked to enter a number.
+        // The user is asked to enter a number.
+        int userNumber = myToolbox.readIntegerFromCmd();
         int sum = 0;
         int integerToSum = 1;
 
         /**
-         * Uses a traditional for loop to output a multiplication table up to 20 times the number specified by the user.
+         * Uses a traditional for loop to output a multiplication table up to 20 times the {@code userNumber}.
          */
         for(int i=1; i<21; i++) {
             System.out.println(userNumber * i);
@@ -27,14 +29,14 @@ public class Main {
 
         /**
          * Uses a while loop to sum consecutive integers starting from 1 (eg. 1+2+3...), until the sum of those integers
-         * is greater than 500. If the sum is grater than 500, then the number of iterations required to reach that number
+         * is greater than 500. If the {@code sum} is grater than 500, then the number of iterations required to reach that number
          * is equal to the last number added - 1.
          */
         while(sum <= 500) {
             sum = sum + integerToSum;
             integerToSum++;
 
-            // When the sum is grater than 500 it prints out the number of iterations required to reach that number.
+            // When the {@code sum} is grater than 500 it prints out the number of iterations required to reach that number.
             if(sum > 500) {
                 System.out.println(integerToSum - 1);
             }
