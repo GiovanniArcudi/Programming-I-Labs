1de5f29d38e9ce15b8c405477b26ebdf95cce563
Giovanni Arcudi <56199876+GiovanniArcudi@users.noreply.github.com>
2019-12-09 08:46:53 +0000

    Update WordGroup.java

--- lab5/lab5part1/WordGroup.java
+++ lab5/lab5part1/WordGroup.java
@@ -1,25 +1,23 @@
 /**
- * This program defines a WordGroup.
- * Every WordGroup object has an instance variable of type String called "words" and some methods to operate with it.
+ * Every {@link WordGroup} object has an instance variable of type String called "words" and some methods to operate with it.
  *
  * @author Giovanni Arcudi
+ * @version 1.0
  */
 public class WordGroup {
     private String words;
 
     /**
-     * This is the WordGroup class constructor.
-     * It takes a String as parameter, then convertes it into lower case using toLowerCase() and stores in the "words"
-     * variable.
+     * This is the {@link WordGroup} class constructor.
+     * It takes a String as parameter, then convertes it into lower case using {@link #toLowerCase()} and stores it in the {@code words}.
      */
     public WordGroup(String input) {
         words = input.toLowerCase();
     }
 
     /**
-     * When the method getWordArray() is called, it returns an array of strings called "separatedWords" of which each
-     * string cointains one single word from the variable "words".
-     * The String class split() method is used to separate words on " ".
+     * Returns the {@code separatedWords} Array of Strings, of which each string cointains one single word from {@code words}.
+     * The String class {@link #split()} method is used to separate words on " ".
      */
     public String[] getWordArray() {
         String[] separatedWords = words.split(" ");
