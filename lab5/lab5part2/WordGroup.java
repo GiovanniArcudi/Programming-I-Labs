c0c82c9ed8677c9c0f860accb59e02e280c6795c
Giovanni Arcudi <56199876+GiovanniArcudi@users.noreply.github.com>
2019-12-09 08:57:37 +0000

    Update WordGroup.java

--- lab5/lab5part2/WordGroup.java
+++ lab5/lab5part2/WordGroup.java
@@ -1,27 +1,27 @@
+import java.util.HashSet;
+
 /**
- * This program defines a WordGroup.
- * Every WordGroup object has an instance variable of type String called "words"and some methods to operate with it.
+ * Every {@link WordGroup} object has an instance variable of type String called "words" and some methods to operate with it.
  *
  * @author Giovanni Arcudi
+ * @version 2.0
  */
-import java.util.HashSet;       // Importing HashSet class library
-
 public class WordGroup {
     private String words;
 
     /**
-     * This is the WordGroup class constructor.
-     * It takes a String as parameter, then convertes it into lower case using toLowerCase() and stores in the "words"
-     * variable.
+     * This is the {@link WordGroup} class constructor.
+     * It takes a String as parameter, then convertes it into lower case using {@link #toLowerCase()} and stores it in the {@code words}.
      */
     public WordGroup(String input) {
         words = input.toLowerCase();
     }
 
     /**
-     * When the method getWordArray() is called, it returns an array of strings called "separatedWords" of which each
-     * string cointains one single word from the variable "words".
-     * The String class split() method is used to separate words on " ".
+     * Returns the {@code separatedWords} Array of Strings, of which each string cointains one single word from {@code words}.
+     * The String class {@link #split()} method is used to separate words on " ".
+     *
+     * @return an array containing each word from words, stored singularly
      */
     public String[] getWordArray() {
         String[] separatedWords = words.split(" ");
@@ -29,21 +29,22 @@ public class WordGroup {
     }
 
     /**
-     * When the method getWordSet() is called, it creates a HashSet<String>, then uses two for loops to put all the words
-     * from this (the object from which the method is called) and the parameter WordGroup into the HashSet, and finally
-     * returns the HashSet<String>.
+     * Creates a HashSet<String>, then uses two for loops to put all the words from this (the object from which the method is called)
+     * and the parameter WordGroup into the HashSet, and finally returns the HashSet<String>.
+     *
+     * @return an HashSet containing all the the words of this WordGroup and the WordGroup passed as parameter
      */
     public HashSet<String> getWordSet(WordGroup wordGroup) {
         HashSet<String> wordsHashSet = new HashSet();
 
         /* Uses two for loops to put all the words from this (remember, you will be executing this method inside a
         WordGroup object) and the parameter WordGroup into the HashSet. */
-        for(int i = 0; i < this.getWordArray().length; i++) {
+        for (int i = 0; i < this.getWordArray().length; i++) {
             String[] wordsArray = this.getWordArray();
             wordsHashSet.add(wordsArray[i]);
         }
 
-        for(int i = 0; i < wordGroup.getWordArray().length; i++) {
+        for (int i = 0; i < wordGroup.getWordArray().length; i++) {
             String[] wordsArray = wordGroup.getWordArray();
             wordsHashSet.add(wordsArray[i]);
         }
