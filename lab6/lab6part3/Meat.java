54d5673d0a71ba39e302d358494f324a6833ed76
Giovanni Arcudi <56199876+GiovanniArcudi@users.noreply.github.com>
2019-12-10 22:24:08 +0000

    Update Meat.java

--- lab6/lab6part3/Meat.java
+++ lab6/lab6part3/Meat.java
@@ -1,15 +1,15 @@
 /**
- * This program defines Meat, extending the Food superclass.
- * Every Meat has a name specified when it is created.
+ * This program defines a Meat object, extending the {@link Food} superclass.
+ * Every Meat has a {@code name} specified when it is created.
  *
  * @author Giovanni Arcudi
+ * @version 3.0
  */
 public class Meat extends Food {
 
-
     /**
-     * This is the Meat class constructor.
-     * It takes a String as parameter and sets it as name of the Meat.
+     * This is the {@link Meat} class constructor.
+     * It takes a String as parameter and sets it as {@code name} of the {@link Meat} object.
      */
     public Meat(String name) {
         super(name);
