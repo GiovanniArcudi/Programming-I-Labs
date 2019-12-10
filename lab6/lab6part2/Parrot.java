588719ad5e52beefb3777efc4ef696e5f4d6b03f
Giovanni Arcudi <56199876+GiovanniArcudi@users.noreply.github.com>
2019-12-10 22:04:39 +0000

    Update Parrot.java

--- lab6/lab6part2/Parrot.java
+++ lab6/lab6part2/Parrot.java
@@ -1,21 +1,22 @@
 /**
- * This program defines a Parrot, extending the Animal superclass.
- * Every Parrot object has a name and an age specified when it is created and a method to make noise.
+ * This program defines a Parrot object, extending the Animal superclass.
+ * Every Parrot object has a {@code name} and an {@code age} specified when it is created and a method to make noise.
  *
  * @author Giovanni Arcudi
+ * @version 2.0
  */
 public class Parrot extends Animal {
 
     /**
-     * This is the Parrot class constructor.
-     * It takes a String and an integer as parameters and sets them as name and age of the Parrot.
+     * This is the {@link Parrot} class constructor.
+     * It takes a String and an int as parameters and sets them as {@code name} and {@code age} of the {@link Parrot}.
      */
     public Parrot(String name, int age) {
         super(name, age);
     }
 
     /**
-     * When the method makeNoise() is called, it prints out the typical Parrot sound.
+     * Prints out the typical Parrot sound.
      */
     public void makeNoise() {
         System.out.println("Chirp!");
