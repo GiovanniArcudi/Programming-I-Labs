3d650f55ec7df6e1f839805233f49231a4c1b5ce
Giovanni Arcudi <56199876+GiovanniArcudi@users.noreply.github.com>
2019-12-10 22:25:30 +0000

    Update Omnivore.java

--- lab6/lab6part3/Omnivore.java
+++ lab6/lab6part3/Omnivore.java
@@ -1,21 +1,22 @@
 /**
- * This program defines an Omnivore, extending the Animal superclass.
- * Every Omnivore object has a name and an age specified when it is created.
+ * This program defines an Omnivore, extending the {@link Animal} superclass.
+ * Every Omnivore object has a {@code name} and an {@code age} specified when it is created.
  *
  * @author Giovanni Arcudi
+ * @version 1.0
  */
 public abstract class Omnivore extends Animal {
 
     /**
-     * This is the Omnivore class constructor.
-     * It takes a String and an integer as parameters and sets them as name and age of the Omnivore.
+     * This is the {@link Omnivore} class constructor.
+     * It takes a String and an integer as parameters and sets them as {@code name} and {@code age} of the new {@link Omnivore}.
      */
     public Omnivore(String name, int age) {
         super(name, age);
     }
 
     /**
-     * When the method eat() is called it prints out a message saying that the animal is eating the given food.
+     * Prints out a message saying that the {@link Animal} is eating the {@link Food} object passed as parameter.
      */
     public void eat(Food food){
         System.out.println("I am eating " + food.getName());
