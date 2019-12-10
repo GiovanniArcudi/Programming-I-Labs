197b899420898499893a72ce59d46899989b2ce5
Giovanni Arcudi <56199876+GiovanniArcudi@users.noreply.github.com>
2019-12-10 22:03:55 +0000

    Update Omnivore.java

--- lab6/lab6part2/Omnivore.java
+++ lab6/lab6part2/Omnivore.java
@@ -1,14 +1,15 @@
 /**
- * This program defines an Omnivore, extending the Animal superclass.
- * Every Omnivore object has a name and an age specified when it is created.
+ * This program defines an Omnivore, extending the {@link Animal} superclass.
+ * Every Omnivore object has a {@code name} and an {@code age} specified when it is created.
  *
  * @author Giovanni Arcudi
+ * @version 1.0
  */
 public abstract class Omnivore extends Animal {
 
     /**
-     * This is the Omnivore class constructor.
-     * It takes a String and an integer as parameters and sets them as name and age of the Omnivore.
+     * This is the {@link Omnivore} class constructor.
+     * It takes a String and an integer as parameters and sets them as {@code name} and {@code age} of the new {@link Omnivore}.
      */
     public Omnivore(String name, int age) {
         super(name, age);
