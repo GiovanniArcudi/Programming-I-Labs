7caae3089cf694b8d455b471dcafa98c01a32fea
Giovanni Arcudi <56199876+GiovanniArcudi@users.noreply.github.com>
2019-12-10 22:15:31 +0000

    Update Carnivore.java

--- lab6/lab6part3/Carnivore.java
+++ lab6/lab6part3/Carnivore.java
@@ -1,22 +1,23 @@
 /**
  * This program defines a Carnivore, extending the Animal superclass.
- * Every Carnivore object has a name and an age specified when it is created.
+ * Every Carnivore object has a {@code name} and an {@code age} specified when it is created.
  *
  * @author Giovanni Arcudi
+ * @version 2.0
  */
 public abstract class Carnivore extends Animal {
 
     /**
      * This is the Carnivore class constructor.
-     * It takes a String and an integer as parameters and sets them as name and age of the Carnivore.
+     * It takes a String and an integer as parameters and sets them as {@code name} and {@code age} of the {@link Carnivore}.
      */
     public Carnivore(String name, int age) {
         super(name, age);
     }
 
     /**
-     * When the method eat() is called, if the food given as parameter is meat, it prints out a message saying that the
-     * animal is eating the given food, else it throws an Exeption.
+     * If the {@link Food} object passed as parameter is an istance of {@link Meat}, it prints out a message saying that
+     * the {@link Animal} is eating the given food, else it throws an Exeption.
      */
     public void eat(Food food) throws Exception {
         if(food instanceof Plant) {
