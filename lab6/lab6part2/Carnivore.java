15244eb92bd6db5e4113fc9b466fd627540f7ad4
Giovanni Arcudi <56199876+GiovanniArcudi@users.noreply.github.com>
2019-12-10 21:56:42 +0000

    Update Carnivore.java

--- lab6/lab6part2/Carnivore.java
+++ lab6/lab6part2/Carnivore.java
@@ -1,14 +1,15 @@
 /**
  * This program defines a Carnivore, extending the Animal superclass.
- * Every Carnivore object has a name and an age specified when it is created.
+ * Every Carnivore object has a {@code name} and an {@code age} specified when it is created.
  *
  * @author Giovanni Arcudi
+ * @version 1.0
  */
 public abstract class Carnivore extends Animal {
 
     /**
      * This is the Carnivore class constructor.
-     * It takes a String and an integer as parameters and sets them as name and age of the Carnivore.
+     * It takes a String and an integer as parameters and sets them as {@code name} and {@code age} of the {@link Carnivore}.
      */
     public Carnivore(String name, int age) {
         super(name, age);
