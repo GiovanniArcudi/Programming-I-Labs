76f88dcc7185f0e22536b7e47c67264bb70a628d
Giovanni Arcudi <56199876+GiovanniArcudi@users.noreply.github.com>
2019-12-12 09:19:47 +0000

    Update Meat.java

--- lab8/lab8part2/Meat.java
+++ lab8/lab8part2/Meat.java
@@ -1,12 +1,14 @@
 /**
- * This program defines Meat, extending the Food superclass.
+ * This program defines a Meat object, extending the {@link Food} superclass.
+ * Every Meat has a {@code name} specified when it is created.
  *
  * @author Giovanni Arcudi
+ * @version 4.0
  */
 public class Meat extends Food {
 
     /**
-     * This is the Meat class constructor.
+     * This is the {@link Meat} class constructor.
      *
      * @param name      the name of the meat
      */
