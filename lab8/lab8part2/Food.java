4d43b2b6533972c9b7c16c5512ce073ed44e031a
Giovanni Arcudi <56199876+GiovanniArcudi@users.noreply.github.com>
2019-12-12 09:15:27 +0000

    Update Food.java

--- lab8/lab8part2/Food.java
+++ lab8/lab8part2/Food.java
@@ -1,13 +1,15 @@
 /**
  * This program defines Food, and is the superclass of all other foods' classes.
+ * Every Food object has a {@code name} specified when it is created, and a relative getter method.
  *
  * @author Giovanni Arcudi
+ * @version 4.0
  */
 public class Food {
     private String name;        // The name of the food.
 
     /**
-     * This is the Food class constructor.
+     * This is the {@link Food} class constructor.
      *
      * @param name      the name of the food
      */
@@ -16,7 +18,7 @@ public class Food {
     }
 
     /**
-     * Food's name accessor method.
+     * Food's {@code name} accessor method.
      *
      * @return the name of the food
      */
