923bc98c147a1b54e340c69519496427cbfd3adc
Giovanni Arcudi <56199876+GiovanniArcudi@users.noreply.github.com>
2019-12-12 09:23:39 +0000

    Update Wolf.java

--- lab8/lab8part2/Wolf.java
+++ lab8/lab8part2/Wolf.java
@@ -1,12 +1,14 @@
 /**
- * This program defines a Wolf, extending the Carnivore superclass.
+ * This program defines a Wolf, extending the {@link Carnivore} superclass.
+ * Every Wolf object has a {@code name} and an {@code age} specified when it is created and a method to make noise.
  *
  * @author Giovanni Arcudi
+ * @version 5.0
  */
 public class Wolf extends Carnivore {
 
     /**
-     * This is the Wolf class constructor.
+     * This is the {@link Wolf} class constructor.
      *
      * @param name      the name of the wolf
      * @param age       the age of the wolf
@@ -21,10 +23,10 @@ public class Wolf extends Carnivore {
     public void makeNoise() {
         System.out.println("Howl!");
     }
-
+    
     /**
-     * This is the second Wolf class constructor.
-     * It sets the age of a new Wolf to 0 and name to "newborn", as specified in the Animal class constructor.
+     * This is the second {@link Wolf} class constructor.
+     * It sets the {@code age} of a new {@link Wolf} to 0 and {@code name} to "newborn".
      */
     public Wolf() {
         super();
