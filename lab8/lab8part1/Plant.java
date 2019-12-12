94283cad651e3f764e78fcd61bd5db950cb2abdd
Giovanni Arcudi <56199876+GiovanniArcudi@users.noreply.github.com>
2019-12-12 09:01:40 +0000

    Update Plant.java

--- lab8/lab8part1/Plant.java
+++ lab8/lab8part1/Plant.java
@@ -1,12 +1,14 @@
 /**
- * This program defines a Plant, extending the Food superclass.
+ * This program defines a Plant, extending the {@link Food} superclass.
+ * Every Plant has a {@code name} specified when it is created.
  *
  * @author Giovanni Arcudi
+ * @version 4.0
  */
 public class Plant extends Food {
 
     /**
-     * This is the Plant class constructor.
+     * This is the {@link Plant} class constructor.
      *
      * @param name      the name of the plant
      */
