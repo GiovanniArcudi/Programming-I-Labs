97f13705a06557d1ebe2440990b97f834eebd601
Giovanni Arcudi <56199876+GiovanniArcudi@users.noreply.github.com>
2019-12-14 12:18:02 +0000

    Update Transport.java

--- lab8/lab8part3/Transport.java
+++ lab8/lab8part3/Transport.java
@@ -1,13 +1,17 @@
 /**
- * This program defines Transport, and is the abstract superclass of all other transport vehicles' classes.
+ * This program defines a mean of transport, and is the abstract superclass of all other transport vehicles' classes.
+ * Every Transport object has a specific nuber of {@code onBoardPassengers} and two methods: {@link #departing(String)}
+ * and {@link #arriving(String)}
  *
  * @author Giovanni Arcudi
+ * @version 1.0
  */
 public abstract class Transport {
     private int onBoardPassengers;      // The number of of passenger on board.
 
     /**
-     *  This is the Transport class constructor.
+     * This is the {@link Transport} class constructor.
+     * It takes an int and sets it as number of {@code onBoardPassengers}.
      *
      * @param onBoardPassengers      the number of passengers on board
      */
@@ -16,18 +20,18 @@ public abstract class Transport {
     }
 
     /**
-     * This abstract method prints out a message saying that the transport vehicle is departing.
+     * This abstract method prints out a message saying that the {@link Transport} vehicle is departing.
      * As different transport vehicles have different ways of moving, each class has to provide its own implementation
-     * for this method.
+     * of this method.
      *
      * @param here              the place from where the transport vehicle is departing
      */
     public abstract void departing(String here);
 
     /**
-     * This abstract method prints out a message saying that the transport vehicle is arriving to destination.
+     * This abstract method prints out a message saying that the {@link Transport} vehicle is arriving to destination.
      * As different transport vehicles have different ways of moving, each class has to provide its own implementation
-     * for this method.
+     * of this method.
      *
      * @param destination       the destination of the journey
      */
