3c5f1278f58fbdcf85178e6ff8e92acc5f85101f
Giovanni Arcudi <56199876+GiovanniArcudi@users.noreply.github.com>
2019-12-14 12:22:21 +0000

    Update Tricycle.java

--- lab8/lab8part3/Tricycle.java
+++ lab8/lab8part3/Tricycle.java
@@ -1,37 +1,41 @@
 /**
  * This program defines a Tricycle, extending the Cycle superclass.
+ * Every Tricycle, apart from the properties and methods inherited from {@link Cycle} has a {@code riderName}.
  *
  * @author Giovanni Arcudi
+ * @version 1.0
  */
 public class Tricycle extends Cycle{
     private String riderName;       // The name of the rider.
 
     /**
-     * This is the Tricycle class constructor.
+     * This is the {@link Tricycle} class constructor.
      *
-     * @param onBoardPassengers the number of passengers on board
+     * @param onBoardPassengers         the number of passengers on board
+     * @param riderName                 the name of the rider
      */
     public Tricycle(int onBoardPassengers, String riderName) {
         super(onBoardPassengers);
         this.riderName = riderName;
     }
 
-    @Override
+   
     /**
-     * Prints out a message saying that the tricycle is departing from a certain location.
+     * Prints out a message saying that the {@link Tricycle} is departing from the location passed as parameter.
      *
      * @param here              the place from where the car is departing
      */
+    @Override
     public void departing(String here) {
         System.out.println("The tricycle ridden by " + riderName + " departing from " + here + " in time.");
     }
 
-    @Override
     /**
-     * Prints out a message saying that the tricycle is arriving to destination.
+     * Prints out a message saying that the {@link Tricycle} is arriving to the destination passed as parameter.
      *
      * @param destination       the destination of the journey
      */
+    @Override
     public void arriving(String destination) {
         System.out.println("The tricycle ridden by " + riderName + " is arriving to " + destination + " in time.");
     }
