a279a025a9baaa44929b861cbae746e934892ee6
Giovanni Arcudi <56199876+GiovanniArcudi@users.noreply.github.com>
2019-12-14 12:08:29 +0000

    Update RoadVehicle.java

--- lab8/lab8part3/RoadVehicle.java
+++ lab8/lab8part3/RoadVehicle.java
@@ -1,14 +1,16 @@
 /**
  * This program defines a Road Vehicle, and is the abstract superclass of all other road vehicles' classes.
+ * It extends the {@link Transport} superclass.
  *
  * @author Giovanni Arcudi
+ * @version 1.0
  */
 public abstract class RoadVehicle extends Transport {
 
     /**
-     * This is the Road Vehicle class constructor.
+     * This is the {@link RoadVehicle} class constructor.
      *
-     * @param onBoardPassengers the number of passengers on board
+     * @param onBoardPassengers         the number of passengers on board
      */
     public RoadVehicle(int onBoardPassengers) {
         super(onBoardPassengers);
