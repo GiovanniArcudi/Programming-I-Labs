0d0723b31f34d9c4a4a3a7a438959920e0b37c1c
Giovanni Arcudi <56199876+GiovanniArcudi@users.noreply.github.com>
2019-12-14 11:53:48 +0000

    Update Cycle.java

--- lab8/lab8part3/Cycle.java
+++ lab8/lab8part3/Cycle.java
@@ -1,12 +1,15 @@
 /**
- * This program defines a Cycle, and is the abstract superclass of all other cycles' classes.
+ * This program defines a Cycle and is the abstract superclass of all other cycles' classes.
+ * It extends the {@link RoadVehicle} superclass.
  *
  * @author Giovanni Arcudi
+ * @version 1.0
  */
 public abstract class Cycle extends RoadVehicle {
 
     /**
-     * This is the Cycle class constructor.
+     * This is the {@link Cycle} class constructor.
+     * It takes an int a sparameter and sets it as {@code onBoardPassengers}.
      *
      * @param onBoardPassengers the number of passengers on board
      */
