d6c00a18a118ab15cd70631642a693957fb27a87
Giovanni Arcudi <56199876+GiovanniArcudi@users.noreply.github.com>
2019-12-14 11:32:03 +0000

    Update Bicycle.java

--- lab8/lab8part3/Bicycle.java
+++ lab8/lab8part3/Bicycle.java
@@ -1,38 +1,44 @@
 /**
- * This program defines a Bicycle, extending the Cycle superclass.
+ * This program defines a Bicycle, extending the {@link Cycle} superclass.
+ * Every Bicycle, apart from the properties inherited by {@link Cycle}, has a String containing the rider name.
  *
  * @author Giovanni Arcudi
+ * @version 1.0
  */
 public class Bicycle extends Cycle{
     private String riderName;       // The name of the rider.
 
     /**
-     * This is the Bicycle class constructor.
+     * This is the {@link Bicycle} class constructor.
+     * It takes an int and a String as parameters and sets them as {@code onBoardPassengers} and {@code riderName} of the
+     * {@link Bicycle} object.
      *
-     * @param onBoardPassengers the number of passengers on board
-     * @param riderName         the name of the rider
+     * @param onBoardPassengers         the number of passengers on board
+     * @param riderName                 the name of the rider
      */
     public Bicycle(int onBoardPassengers, String riderName) {
         super(onBoardPassengers);
         this.riderName = riderName;
     }
 
-    @Override
+
     /**
-     * Prints out a message saying that the bike is departing from a certain location.
+     * Prints out a message saying that the {@link Bicycle} is departing from the location passed as parameter.
      *
-     * @param here              the place from where the car is departing
-     */
+     * @param here        the place from where the car is departing
+     */   
+    @Override
     public void departing(String here) {
         System.out.println("The bike ridden by " + riderName + " departing from " + here + " in time.");
     }
 
-    @Override
+
     /**
-     * Prints out a message saying that the bike is arriving to destination.
+     * Prints out a message saying that the bike is arriving to the destination passed as parameter.
      *
      * @param destination       the destination of the journey
-     */
+     */    
+    @Override
     public void arriving(String destination) {
         System.out.println("The bike ridden by " + riderName + " is arriving to " + destination + " in time.");
     }
